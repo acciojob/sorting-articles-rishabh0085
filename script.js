2e0afe46cb4e366fb00580155fe5ba899c4f83f2
Rishabh Singh <52191143+rishabh0085@users.noreply.github.com>
2024-03-23 18:55:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled'];
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
-// Function to sort articles without considering specified words
-function sortArticles(articles) {
-  return articles.sort((a, b) => {
-    // Function to remove specified words from article title
-    const removeWords = str => str.replace(/^(a|an|the)\s+/i, '');
-    // Compare articles without specified words
-    return removeWords(a) > removeWords(b) ? 1 : -1;
-  });
-}
-
-// Function to display sorted articles in the unordered list
-function displayArticles(articles) {
-  const ul = document.getElementById('band');
-  ul.innerHTML = ''; // Clear existing list
-  articles.forEach(article => {
-    const li = document.createElement('li');
-    li.textContent = article;
-    ul.appendChild(li);
-  });
-}
-
-// Sort articles and display them
-const sortedArticles = sortArticles(bands);
-displayArticles(sortedArticles);
+let unorderValue = document.querySelector("#band");
